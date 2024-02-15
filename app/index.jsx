46d5070cc6fbf504caf1ee628c8508186250a979
sreehari jayaraj <sreeharivijaya2003@gmail.com>
2024-02-15 23:42:04 +0530

    feat: mobile app

---
+++ app/index.jsx
@@ -0,0 +1,33 @@
+import { View } from "react-native";
+import { PanGestureHandler } from "react-native-gesture-handler";
+
+import { OpenPage } from "../src/components/OpenPage";
+import { Qrcode } from "../src/components/QrCode";
+import { useToggle } from "../src/hooks";
+import { QrToggler } from "../src/views/home/QrToggler";
+
+const Index = () => {
+  const [isQrCodeShowing, toggleQr] = useToggle(false);
+
+  const onGestureEvent = (event) => {
+    const translationX = event.nativeEvent.translationX;
+
+    if (translationX > 50) {
+      toggleQr.off();
+    } else if (translationX < -50) {
+      toggleQr.on();
+    }
+  };
+
+  return (
+    <PanGestureHandler onGestureEvent={onGestureEvent}>
+      <View style={{ flex: 1 }}>
+        {!isQrCodeShowing && <OpenPage />}
+        {isQrCodeShowing && <Qrcode />}
+        <QrToggler toggleQr={toggleQr} />
+      </View>
+    </PanGestureHandler>
+  );
+};
+
+export default Index;
